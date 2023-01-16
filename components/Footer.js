d373f56b949429107c9afb68e84177ea2c23e4c8
Joseph Archer <viewtiful.kja@gmail.com>
2023-01-16 11:43:23 -0500

    Finished homepage, addeding individual product page layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { RiInstagramFill, RiTwitterFill } from 'react-icons/ri'
+
+import moment from 'moment'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer-container'>
+            <p>
+                {moment().year()} L-Active All rights reserved
+            </p>
+            <p className='icons'>
+                <RiInstagramFill />
+                <RiTwitterFill />
+            </p>
+        </div>
     )
 }
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link';
+
+import { HiOutlineShoppingBag } from 'react-icons/hi'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar-container'>
+            <p className='logo'>
+                <Link href={`/`}>
+                    L-Active Wear
+                </Link>
+            </p>
+
+            <button type='button' className='cart-icon' onClick=''>
+                <HiOutlineShoppingBag />
+                <span className='cart-item-qty'>1</span>
+            </button>
+        </div>
     )
 }
 
